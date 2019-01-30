f5d8e0f86108644fc8d6d792dee22667772e71e1
Unknown <CIDJEY1991@mail.ru>
2019-01-30 11:37:35 +0300

    hello world test

--- ci/test_rollback.cpp
+++ ci/test_rollback.cpp
@@ -9,20 +9,18 @@ using namespace fakeit;
 TEST_CASE("rollback") {
 	fs::path src_file("test_rollback.testo");
 
-	if (src_file.is_relative()) {
-		src_file = fs::canonical(src_file);
-	}
-
 	Mock<Environment> mock_env;
 	Mock<VmController> mock_vm;
 
-	When(Method(mock_env, create_vm_controller)).Return(&mock_vm.get());
+	When(Method(mock_env, create_vm_controller)).Return(std::shared_ptr<VmController>(&mock_vm.get(), [](VmController*){}));
 	Fake(Method(mock_env, setup));
 	Fake(Method(mock_env, cleanup));
 
 	Fake(Method(mock_vm, unplug_all_flash_drives));
-	Interpreter runner(mock_env.get(), src_file);
-	runner.run();
+	{
+		Interpreter runner(mock_env.get(), src_file);
+		runner.run();
+	}
 
-	Verify(Method(mock_vm, setup)).AtLeastOnce();
+	REQUIRE_NOTHROW(Verify(Method(mock_vm, unplug_all_flash_drives)).AtLeastOnce());
 }
