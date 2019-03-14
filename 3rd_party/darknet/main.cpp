a721a6c6f423a97f43750e1ae451b77d0c145bb5
Unknown <log0div0@gmail.com>
2019-03-14 11:45:13 +0300

    darknet main.cpp

---
+++ 3rd_party/darknet/main.cpp
@@ -0,0 +1,210 @@
+#include "darknet.h"
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void train_detector(char *cfgfile, char *weightfile, int *gpus, int ngpus, int clear)
+{
+	char *train_images = "data/train.list";
+	char *backup_directory = "/backup/";
+
+	srand(time(0));
+	char *base = basecfg(cfgfile);
+	printf("%s\n", base);
+	float avg_loss = -1;
+	network **nets = (network**)calloc(ngpus, sizeof(network));
+
+	srand(time(0));
+	int seed = rand();
+	int i;
+	for(i = 0; i < ngpus; ++i){
+		srand(seed);
+#ifdef GPU
+		cuda_set_device(gpus[i]);
+#endif
+		nets[i] = load_network(cfgfile, weightfile, clear);
+		nets[i]->learning_rate *= ngpus;
+	}
+	srand(time(0));
+	network *net = nets[0];
+
+	int imgs = net->batch * net->subdivisions * ngpus;
+	printf("Learning Rate: %g, Momentum: %g, Decay: %g\n", net->learning_rate, net->momentum, net->decay);
+	data train, buffer;
+
+	layer l = net->layers[net->n - 1];
+
+	int classes = l.classes;
+	float jitter = l.jitter;
+
+	list *plist = get_paths(train_images);
+	char **paths = (char **)list_to_array(plist);
+
+	load_args args = get_base_args(net);
+	args.coords = l.coords;
+	args.paths = paths;
+	args.n = imgs;
+	args.m = plist->size;
+	args.classes = classes;
+	args.jitter = jitter;
+	args.num_boxes = l.max_boxes;
+	args.d = &buffer;
+	args.type = DETECTION_DATA;
+	args.threads = 64;
+
+	pthread_t load_thread = load_data(args);
+	double time;
+	while(get_current_batch(net) < net->max_batches){
+		time=what_time_is_it_now();
+		pthread_join(load_thread, 0);
+		train = buffer;
+		load_thread = load_data(args);
+
+		printf("Loaded: %lf seconds\n", what_time_is_it_now()-time);
+
+		time=what_time_is_it_now();
+		float loss = 0;
+#ifdef GPU
+		if(ngpus == 1){
+			loss = train_network(net, train);
+		} else {
+			loss = train_networks(nets, ngpus, train, 4);
+		}
+#else
+		loss = train_network(net, train);
+#endif
+		if (avg_loss < 0) avg_loss = loss;
+		avg_loss = avg_loss*.9 + loss*.1;
+
+		i = get_current_batch(net);
+		printf("%ld: %f, %f avg, %f rate, %lf seconds, %d images\n", get_current_batch(net), loss, avg_loss, get_current_rate(net), what_time_is_it_now()-time, i*imgs);
+		if(i%100==0){
+#ifdef GPU
+			if(ngpus != 1) sync_nets(nets, ngpus, 0);
+#endif
+			char buff[256];
+			sprintf(buff, "%s/%s.backup", backup_directory, base);
+			save_weights(net, buff);
+		}
+		if(i%10000==0 || (i < 1000 && i%100 == 0)){
+#ifdef GPU
+			if(ngpus != 1) sync_nets(nets, ngpus, 0);
+#endif
+			char buff[256];
+			sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
+			save_weights(net, buff);
+		}
+		free_data(train);
+	}
+#ifdef GPU
+	if(ngpus != 1) sync_nets(nets, ngpus, 0);
+#endif
+	char buff[256];
+	sprintf(buff, "%s/%s_final.weights", backup_directory, base);
+	save_weights(net, buff);
+}
+
+
+void test_detector(char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen)
+{
+	network *net = load_network(cfgfile, weightfile, 0);
+	set_batch_network(net, 1);
+	srand(2222222);
+	double time;
+	char buff[256];
+	char *input = buff;
+	float nms=.45;
+	while(1){
+		if(filename){
+			strncpy(input, filename, 256);
+		} else {
+			printf("Enter Image Path: ");
+			fflush(stdout);
+			input = fgets(input, 256, stdin);
+			if(!input) return;
+			strtok(input, "\n");
+		}
+		image im = load_image_color(input,0,0);
+		image sized = letterbox_image(im, net->w, net->h);
+		layer l = net->layers[net->n-1];
+
+
+		float *X = sized.data;
+		time=what_time_is_it_now();
+		network_predict(net, X);
+		printf("%s: Predicted in %f seconds.\n", input, what_time_is_it_now()-time);
+		int nboxes = 0;
+		detection *dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes);
+		if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
+		draw_detections(im, dets, nboxes, thresh, l.classes);
+		free_detections(dets, nboxes);
+		if(outfile){
+			save_image(im, outfile);
+		}
+		else{
+			save_image(im, "predictions");
+		}
+
+		free_image(im);
+		free_image(sized);
+		if (filename) break;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	gpu_index = find_int_arg(argc, argv, "-i", 0);
+	if (find_arg(argc, argv, "-nogpu")) {
+		gpu_index = -1;
+	}
+
+#ifndef GPU
+	gpu_index = -1;
+#else
+	if (gpu_index >= 0) {
+		cuda_set_device(gpu_index);
+	}
+#endif
+
+	float thresh = find_float_arg(argc, argv, "-thresh", .5);
+	float hier_thresh = find_float_arg(argc, argv, "-hier", .5);
+	if(argc < 3){
+		fprintf(stderr, "usage: %s [train/test] [cfg] [weights (optional)]\n", argv[0]);
+		return 1;
+	}
+	char *gpu_list = find_char_arg(argc, argv, "-gpus", 0);
+	char *outfile = find_char_arg(argc, argv, "-out", 0);
+	int *gpus = 0;
+	int gpu = 0;
+	int ngpus = 0;
+	if(gpu_list){
+		printf("%s\n", gpu_list);
+		int len = strlen(gpu_list);
+		ngpus = 1;
+		int i;
+		for(i = 0; i < len; ++i){
+			if (gpu_list[i] == ',') ++ngpus;
+		}
+		gpus = (int*)calloc(ngpus, sizeof(int));
+		for(i = 0; i < ngpus; ++i){
+			gpus[i] = atoi(gpu_list);
+			gpu_list = strchr(gpu_list, ',')+1;
+		}
+	} else {
+		gpu = gpu_index;
+		gpus = &gpu;
+		ngpus = 1;
+	}
+
+	int clear = find_arg(argc, argv, "-clear");
+	int fullscreen = find_arg(argc, argv, "-fullscreen");
+
+	char *cfg = argv[2];
+	char *weights = (argc > 3) ? argv[3] : 0;
+	char *filename = (argc > 4) ? argv[4]: 0;
+	if(0==strcmp(argv[1], "test")) test_detector(cfg, weights, filename, thresh, hier_thresh, outfile, fullscreen);
+	else if(0==strcmp(argv[1], "train")) train_detector(cfg, weights, gpus, ngpus, clear);
+
+	return 0;
+}
--- 3rd_party/darknet/src/image.c
+++ 3rd_party/darknet/src/image.c
@@ -81,8 +81,8 @@ static float bilinear_interpolate(image im, float x, float y, int c)
     float dx = x - ix;
     float dy = y - iy;
 
-    float val = (1-dy) * (1-dx) * get_pixel_extend(im, ix, iy, c) + 
-        dy     * (1-dx) * get_pixel_extend(im, ix, iy+1, c) + 
+    float val = (1-dy) * (1-dx) * get_pixel_extend(im, ix, iy, c) +
+        dy     * (1-dx) * get_pixel_extend(im, ix, iy+1, c) +
         (1-dy) *   dx   * get_pixel_extend(im, ix+1, iy, c) +
         dy     *   dx   * get_pixel_extend(im, ix+1, iy+1, c);
     return val;
@@ -124,7 +124,7 @@ image tile_images(image a, image b, int dx)
     if(a.w == 0) return copy_image(b);
     image c = make_image(a.w + b.w + dx, (a.h > b.h) ? a.h : b.h, (a.c > b.c) ? a.c : b.c);
     fill_cpu(c.w*c.h*c.c, 1, c.data, 1);
-    embed_image(a, c, 0, 0); 
+    embed_image(a, c, 0, 0);
     composite_image(b, c, a.w + dx, 0);
     return c;
 }
@@ -236,23 +236,17 @@ image **load_alphabet()
     return alphabets;
 }
 
-void draw_detections(image im, detection *dets, int num, float thresh, char **names, image **alphabet, int classes)
+void draw_detections(image im, detection *dets, int num, float thresh, int classes)
 {
     int i,j;
 
     for(i = 0; i < num; ++i){
-        char labelstr[4096] = {0};
         int class = -1;
         for(j = 0; j < classes; ++j){
             if (dets[i].prob[j] > thresh){
                 if (class < 0) {
-                    strcat(labelstr, names[j]);
                     class = j;
-                } else {
-                    // strcat(labelstr, ", ");
-                    strcat(labelstr, names[j]);
                 }
-                printf("%s: %.0f%%\n", names[j], dets[i].prob[j]*100);
             }
         }
         if(class >= 0){
@@ -291,11 +285,6 @@ void draw_detections(image im, detection *dets, int num, float thresh, char **na
             if(bot > im.h-1) bot = im.h-1;
 
             draw_box_width(im, left, top, right, bot, width, red, green, blue);
-            if (alphabet) {
-                image label = get_label(alphabet, labelstr, 8);
-                draw_label(im, top + width, left, label, rgb);
-                free_image(label);
-            }
             if (dets[i].mask){
                 image mask = float_to_image(14, 14, 1, dets[i].mask);
                 image resized_mask = resize_image(mask, b.w*im.w, b.h*im.h);
@@ -647,7 +636,7 @@ void place_image(image im, int w, int h, int dx, int dy, image canvas)
 
 image center_crop_image(image im, int w, int h)
 {
-    int m = (im.w < im.h) ? im.w : im.h;   
+    int m = (im.w < im.h) ? im.w : im.h;
     image c = crop_image(im, (im.w - m) / 2, (im.h - m)/2, m, m);
     image r = resize_image(c, w, h);
     free_image(c);
@@ -805,7 +794,7 @@ void letterbox_image_into(image im, int w, int h, image boxed)
         new_w = (im.w * h)/im.h;
     }
     image resized = resize_image(im, new_w, new_h);
-    embed_image(resized, boxed, (w-new_w)/2, (h-new_h)/2); 
+    embed_image(resized, boxed, (w-new_w)/2, (h-new_h)/2);
     free_image(resized);
 }
 
@@ -825,7 +814,7 @@ image letterbox_image(image im, int w, int h)
     fill_image(boxed, .5);
     //int i;
     //for(i = 0; i < boxed.w*boxed.h*boxed.c; ++i) boxed.data[i] = 0;
-    embed_image(resized, boxed, (w-new_w)/2, (h-new_h)/2); 
+    embed_image(resized, boxed, (w-new_w)/2, (h-new_h)/2);
     free_image(resized);
     return boxed;
 }
@@ -1091,7 +1080,7 @@ image blend_image(image fore, image back, float alpha)
     for(k = 0; k < fore.c; ++k){
         for(j = 0; j < fore.h; ++j){
             for(i = 0; i < fore.w; ++i){
-                float val = alpha * get_pixel(fore, i, j, k) + 
+                float val = alpha * get_pixel(fore, i, j, k) +
                     (1 - alpha)* get_pixel(back, i, j, k);
                 set_pixel(blend, i, j, k, val);
             }
@@ -1198,7 +1187,7 @@ void saturate_exposure_image(image im, float sat, float exposure)
 
 image resize_image(image im, int w, int h)
 {
-    image resized = make_image(w, h, im.c);   
+    image resized = make_image(w, h, im.c);
     image part = make_image(w, im.h, im.c);
     int r, c, k;
     float w_scale = (float)(im.w - 1) / (w - 1);
@@ -1394,7 +1383,7 @@ image collapse_images_vert(image *ims, int n)
         free_image(copy);
     }
     return filters;
-} 
+}
 
 image collapse_images_horz(image *ims, int n)
 {
@@ -1430,7 +1419,7 @@ image collapse_images_horz(image *ims, int n)
         free_image(copy);
     }
     return filters;
-} 
+}
 
 void show_image_normalized(image im, const char *name)
 {
