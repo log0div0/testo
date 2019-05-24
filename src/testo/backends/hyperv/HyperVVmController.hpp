11df2a8ecc854f22560b5b0b8dc6e9445a20998a
Unknown <log0div0@gmail.com>
2019-05-24 11:23:39 +0300

    screenshot

--- src/testo/backends/hyperv/HyperVVmController.hpp
+++ src/testo/backends/hyperv/HyperVVmController.hpp
@@ -31,8 +31,7 @@ struct HyperVVmController: VmController {
 	void resume() override;
 	void shutdown(uint32_t timeout_seconds) override;
 	void type(const std::string& text) override;
-	bool wait(const std::string& text, const nlohmann::json& params, const std::string& time) override;
-	bool check(const std::string& text, const nlohmann::json& params) override;
+	stb::Image screenshot() override;
 	int run(const fs::path& exe, std::vector<std::string> args, uint32_t timeout_seconds) override;
 
 	bool is_flash_plugged(std::shared_ptr<FlashDriveController> fd) override;
