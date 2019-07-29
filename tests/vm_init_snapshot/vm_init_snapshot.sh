fcf6491fe18815168e4bca92bf24864be12c9932
Unknown <CIDJEY1991@mail.ru>
2019-07-29 13:49:56 +0300

    vm_init_snapshot test - FAIL

---
+++ tests/vm_init_snapshot/vm_init_snapshot.sh
@@ -0,0 +1,58 @@
+
+BASEDIR=$(dirname "$0")
+
+TESTO_BIN=$SBIN_DIR/testo
+
+assert_output() {
+	if [ "$1" != "$2" ]; then
+		diff <(echo "$1") <(echo "$2")
+		exit 1
+	fi
+}
+
+rm -rf ./dummy_hypervisor_files
+rm -rf ./flash_drives_metadata
+rm -rf ./vm_metadata
+
+mkdir ./dummy_hypervisor_files
+
+OUTPUT=`$TESTO_BIN $BASEDIR/scripts/base.testo --hypervisor dummy`
+
+echo "$OUTPUT"
+
+EVERYTHING_PASSED="Registering machine my_machine
+TEST TO RUN
+test1
+test2
+[  0%] Preparing the environment for the test test1
+[  0%] Creating machine my_machine
+[  0%] Running test test1
+[  0%] my_machine: test1
+[  0%] Taking snapshot test1 for entity my_machine
+[ 50%] Test test1 PASSED
+[ 50%] Preparing the environment for the test test2
+[ 50%] Creating machine my_machine
+[ 50%] Running test test2
+[ 50%] my_machine: test2
+[ 50%] Taking snapshot test2 for entity my_machine
+[100%] Test test2 PASSED
+PROCESSED TOTAL 2 TESTS IN 0h:0m:0s
+UP TO DATE: 0
+RUN SUCCESSFULLY: 2
+FAILED: 0"
+
+assert_output "$OUTPUT" "$EVERYTHING_PASSED"
+
+OUTPUT=`$TESTO_BIN $BASEDIR/scripts/base.testo --hypervisor dummy`
+
+EVERYTHING_UP_TO_DATE="Registering machine my_machine
+Registering flash my_flash
+[ 50%] Test test1 is up-to-date, skipping...
+[100%] Test test2 is up-to-date, skipping...
+TEST TO RUN
+PROCESSED TOTAL 2 TESTS IN 0h:0m:0s
+UP TO DATE: 2
+RUN SUCCESSFULLY: 0
+FAILED: 0"
+
+assert_output "$OUTPUT" "$EVERYTHING_UP_TO_DATE"
