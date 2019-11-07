b8f28c64547ba03ba75155b3037b8ffb2ec6b6d2
Sergey Lvov <s.lvov@securitycode.ru>
2019-11-07 23:13:04 +0300

    focal loss

--- src/nn/text_detector/train.py
+++ src/nn/text_detector/train.py
@@ -34,6 +34,25 @@ model = Model()
 
 optimizer = tf.keras.optimizers.Adam(lr=1e-3)
 
+K = tf.keras.backend
+
+def binary_focal_loss(y_true, y_pred, gamma=2.):
+	pt = y_pred * y_true + (1 - y_pred) * (1 - y_true)
+	epsilon = K.epsilon()
+	pt = K.clip(pt, epsilon, 1. - epsilon)
+	cross_entropy = -K.log(pt)
+	loss = K.pow(1 - pt, gamma) * cross_entropy
+	return K.sum(loss, axis=-1)
+
+def categorical_focal_loss(y_true, y_pred, gamma=2.):
+	y_true = K.squeeze(y_true, axis=-1)
+	y_true = K.one_hot(tf.cast(y_true, tf.int32), num_classes=tf.shape(y_pred)[-1])
+	epsilon = K.epsilon()
+	y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+	cross_entropy = -y_true * K.log(y_pred)
+	loss = K.pow(1 - y_pred, gamma) * cross_entropy
+	return K.sum(loss, axis=-1)
+
 def Loss(y_true, y_pred):
 	pred_obj, pred_xy, pred_wh, pred_symbol, pred_fg, pred_bg = tf.split(
 		y_pred, (1, 2, 2, len(symbols), len(colors), len(colors)), axis=-1)
@@ -41,7 +60,7 @@ def Loss(y_true, y_pred):
 	true_obj, true_xy, true_wh, true_symbol, true_fg, true_bg = tf.split(
 		y_true, (1, 2, 2, 1, 1, 1), axis=-1)
 
-	obj_loss = tf.keras.losses.binary_crossentropy(true_obj, pred_obj)
+	obj_loss = binary_focal_loss(true_obj, pred_obj)
 
 	grid_size = tf.cast([grid_w, grid_h], tf.float32)
 	grid = tf.meshgrid(tf.range(grid_w), tf.range(grid_h))
@@ -54,9 +73,9 @@ def Loss(y_true, y_pred):
 	obj_mask = tf.squeeze(true_obj, -1)
 	xy_loss = obj_mask * tf.reduce_sum(tf.square(true_xy - pred_xy), axis=-1)
 	wh_loss = obj_mask * tf.reduce_sum(tf.square(true_wh - pred_wh), axis=-1)
-	symbol_loss = obj_mask * tf.keras.losses.sparse_categorical_crossentropy(true_symbol, pred_symbol)
-	fg_loss = obj_mask * tf.keras.losses.sparse_categorical_crossentropy(true_fg, pred_fg)
-	bg_loss = obj_mask * tf.keras.losses.sparse_categorical_crossentropy(true_bg, pred_bg)
+	symbol_loss = obj_mask * categorical_focal_loss(true_symbol, pred_symbol)
+	fg_loss = obj_mask * categorical_focal_loss(true_fg, pred_fg)
+	bg_loss = obj_mask * categorical_focal_loss(true_bg, pred_bg)
 
 	return obj_loss + xy_loss + wh_loss + symbol_loss + fg_loss + bg_loss
 
@@ -67,5 +86,5 @@ callbacks = [
 	tf.keras.callbacks.ModelCheckpoint('checkpoints/epoch_{epoch}.tf', verbose=1, save_weights_only=True),
 ]
 
-model.fit(dataset, epochs=20, callbacks=callbacks)
+model.fit(dataset, epochs=40, callbacks=callbacks)
 model.save_weights('checkpoints/final.tf')
