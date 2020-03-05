631373cb7c356bdcedd3c78a69545e4716ef1f60
Unknown <CIDJEY1991@mail.ru>
2020-03-05 19:50:40 +0300

    ckusm update

--- src/testo/VisitorCksum.hpp
+++ src/testo/VisitorCksum.hpp
@@ -28,6 +28,7 @@ struct VisitorCksum {
 	std::string visit_copy(std::shared_ptr<VmController> vmc, std::shared_ptr<AST::Copy> copy);
 	std::string visit_macro_call(std::shared_ptr<VmController> vmc, std::shared_ptr<AST::MacroCall> macro_call);
 	std::string visit_if_clause(std::shared_ptr<VmController> vmc, std::shared_ptr<AST::IfClause> if_clause);
+	std::string visit_range(std::shared_ptr<AST::Range> range);
 	std::string visit_for_clause(std::shared_ptr<VmController> vmc, std::shared_ptr<AST::ForClause> for_clause);
 
 	std::string visit_expr(std::shared_ptr<VmController> vmc, std::shared_ptr<AST::IExpr> expr);
