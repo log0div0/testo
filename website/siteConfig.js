fc1fff23b42289505a572992fda1a54cd6e30d9e
Unknown <CIDJEY1991@mail.ru>
2020-04-14 16:45:39 +0300

    First tutorial. Creating VM

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -42,6 +42,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'getting_started/intro', label: 'Docs'},
+    {doc: 'tutorials/1_creating_vm', label: 'Обучающие материалы'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
