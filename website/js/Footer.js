3d990d9a9c2e066f2694e32655723e0b7e2daefd
Sergey Lvov <log0div0@gmail.com>
2020-04-27 18:08:09 +0300

    header/footer

--- website/js/Footer.js
+++ website/js/Footer.js
@@ -3,34 +3,14 @@ import React from 'react'
 
 module.exports = function() {
 	return (
-		<div className="nav-footer">
-			<section className="sitemap">
+		<footer>
+			<nav>
+				<h1>Наш канал на YouTube</h1>
 				<div>
-					<a href="/" className="nav-home">
-						<img src="/static/logo_v.svg" alt="Testo Lang"/>
-					</a>
+					<script src="https://apis.google.com/js/platform.js"></script>
+					<div className="g-ytsubscribe" data-channelid="UC4voSBtFRjRE4V1gzMZoZuA" data-layout="default" data-count="default"></div>
 				</div>
-				<div>
-					<h5>Контакты</h5>
-					<a href="#">Ссылка 1</a>
-					<a href="#">Ссылка 2</a>
-					<a href="#">Ссылка 3</a>
-					<a href="#">Ссылка 4</a>
-				</div>
-				<div>
-					<h5>Помощь</h5>
-					<a href="#">Ссылка 1</a>
-					<a href="#">Ссылка 2</a>
-					<a href="#">Ссылка 3</a>
-				</div>
-				<div>
-					<h5>Подписывайтесь на нас</h5>
-					<div className="social">
-						<script src="https://apis.google.com/js/platform.js"></script>
-						<div className="g-ytsubscribe" data-channelid="UC4voSBtFRjRE4V1gzMZoZuA" data-layout="default" data-count="default"></div>
-					</div>
-				</div>
-			</section>
-		</div>
+			</nav>
+		</footer>
 	)
 }
--- website/js/Header.js
+++ website/js/Header.js
@@ -3,32 +3,20 @@ import React from 'react'
 
 module.exports = function() {
 	return (
-		<div className="fixedHeaderContainer">
-			<div className="wrapper">
-				<header>
+		<header>
+			<nav>
+				<section>
 					<a href="/">
-						<h2>Testo Lang</h2>
+						<h1>Testo Lang</h1>
 					</a>
-					<div className="navigationSlider">
-						<nav className="slidingNav">
-							<ul>
-								<li>
-									<a href="#">Загрузить</a>
-								</li>
-								<li>
-									<a href="#">Купить</a>
-								</li>
-								<li id="nav-site-docs">
-									<a href="/docs/getting_started/intro">Документация</a>
-								</li>
-								<li id="nav-site-tutorials">
-									<a href="/tutorials/1_creating_vm">Обучающие материалы</a>
-								</li>
-							</ul>
-						</nav>
-					</div>
-				</header>
-			</div>
-		</div>
+				</section>
+				<section>
+					<a href="#">Загрузить</a>
+					<a href="#">Купить</a>
+					<a href="/docs/getting_started/intro">Документация</a>
+					<a href="/tutorials/1_creating_vm">Обучающие материалы</a>
+				</section>
+			</nav>
+		</header>
 	)
 }
