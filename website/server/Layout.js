727385cfe275445c6a8d4802d7ef120915ca002c
Sergey Lvov <log0div0@gmail.com>
2020-04-30 14:23:56 +0300

    roboto font

--- website/server/Layout.js
+++ website/server/Layout.js
@@ -8,6 +8,7 @@ module.exports = function({children}) {
 		<html>
 			<head>
 				<title>Testo Lang</title>
+				<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet"/>
 				<link rel="stylesheet" href="/main.css" type="text/css"/>
 			</head>
 			<body>
