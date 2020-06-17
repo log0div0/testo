e2b3abf318e814bf532dc5d5497d9f45484e232d
Sergey Lvov <log0div0@gmail.com>
2020-06-17 19:27:07 +0300

    build_onnxruntime.sh

--- build/build.sh
+++ build/build.sh
@@ -6,17 +6,20 @@ SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 TESTO_SRC_DIR=$(readlink -f "$SCRIPT_DIR/..")
 OUT_DIR=$(readlink -f "$SCRIPT_DIR/out")
+TMP_DIR=$(readlink -f "$SCRIPT_DIR/tmp")
 ISO_DIR=$(readlink -f "$TESTO_SRC_DIR/../iso")
 LICENSE_PATH=$(readlink -f "$TESTO_SRC_DIR/../testo_license")
 
 mkdir -p "$OUT_DIR"
+mkdir -p "$TMP_DIR"
 
 sudo testo run "$SCRIPT_DIR/src" \
 	--stop_on_fail \
 	--prefix tb_ \
 	--param ISO_DIR "$ISO_DIR" \
 	--param TESTO_SRC_DIR "$TESTO_SRC_DIR" \
+	--param TMP_DIR "$TMP_DIR" \
 	--param OUT_DIR "$OUT_DIR" \
 	--param TESTO_VERSION "$TESTO_VERSION" \
 	--license "$LICENSE_PATH" \
-	--test_spec centos_install_guest_additions
+	--test_spec $1
---
+++ build/build_onnxruntime.sh
@@ -0,0 +1,3 @@
+
+SCRIPT_DIR=$(dirname "$0")
+$SCRIPT_DIR/build.sh *build_onnxruntime*
