4d8970a9b703db7f8d97fe757a312621c566aa55
Sergey Lvov <log0div0@gmail.com>
2020-06-17 17:03:29 +0300

    всё, что связано со сборкой - в каталоге build

--- build/build.sh
+++ build/build.sh
@@ -3,18 +3,20 @@ set -euo pipefail
 
 TESTO_VERSION="1.6.0"
 SCRIPT_PATH=$(readlink -f "$0")
-TESTO_SRC_DIR=$(dirname "$SCRIPT_PATH")
-TESTO_DIST_DIR=$(readlink -f "$TESTO_SRC_DIR/out")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+TESTO_SRC_DIR=$(readlink -f "$SCRIPT_DIR/..")
+OUT_DIR=$(readlink -f "$SCRIPT_DIR/out")
 ISO_DIR=$(readlink -f "$TESTO_SRC_DIR/../iso")
 LICENSE_PATH=$(readlink -f "$TESTO_SRC_DIR/../testo_license")
 
-mkdir -p "$TESTO_DIST_DIR"
+mkdir -p "$OUT_DIR"
 
-sudo testo run "$TESTO_SRC_DIR/build" \
+sudo testo run "$SCRIPT_DIR/src" \
 	--stop_on_fail \
 	--prefix tb_ \
 	--param ISO_DIR "$ISO_DIR" \
 	--param TESTO_SRC_DIR "$TESTO_SRC_DIR" \
-	--param TESTO_DIST_DIR "$TESTO_DIST_DIR" \
+	--param OUT_DIR "$OUT_DIR" \
 	--param TESTO_VERSION "$TESTO_VERSION" \
-	--license "$LICENSE_PATH"
+	--license "$LICENSE_PATH" \
+	--test_spec centos_install_guest_additions
