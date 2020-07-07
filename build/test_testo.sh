5ccc5711d45a45c4339454df730642f44aa43030
Unknown <CIDJEY1991@mail.ru>
2020-07-07 19:32:54 +0300

    build nbd module in CentOS testing machine

--- build/test_testo.sh
+++ build/test_testo.sh
@@ -2,9 +2,10 @@
 
 source "$(dirname "$0")/vars.sh"
 
-sudo testo run "$SCRIPT_DIR/../tests" \
+sudo testo run "$SCRIPT_DIR/../tests/running_tests" \
 	--stop_on_fail \
 	--prefix tt_ \
 	--param ISO_DIR "$ISO_DIR" \
 	--param TESTO_BUILD_DIR "$OUT_DIR" \
+	--param TESTO_TESTS_DIR "$TESTO_TESTS_DIR" \
 	--license "$LICENSE_PATH"
