57afaa1dce5f66eec873c39776a97f1db39f5635
Sergey Lvov <log0div0@gmail.com>
2020-07-28 12:31:28 +0300

    fix license request messages

--- src/testo/ModeRequestLicense.cpp
+++ src/testo/ModeRequestLicense.cpp
@@ -10,6 +10,10 @@ namespace fs = std::experimental::filesystem;
 #ifdef USE_CUDA
 
 int request_license_mode(const RequestLicenseModeArgs& args) {
+	auto path_to_save = fs::absolute(args.out).generic_string();
+	if (fs::exists(path_to_save)) {
+		throw std::runtime_error("Error: The file \"" + path_to_save + "\" already exists");
+	}
 	std::cout << "Checking that the system meets the requirements ..." << std::endl;
 	nn::OnnxRuntime runtime;
 	runtime.selftest();
@@ -19,13 +23,9 @@ int request_license_mode(const RequestLicenseModeArgs& args) {
 		{"device_uuid", info.uuid_str}
 	};
 	std::string container = license::pack(request, "QpCtaVw2J4EsVcBlp0KH5VA5GWXpHMdpTQqR8vCoMzIrh8MiA8wr8X8IWi4mbhvjLRLrS8QIs6Gw0ZiSmQXIBA==");
-	auto path_to_save = fs::absolute(args.out).generic_string();
-	if (fs::exists(path_to_save)) {
-		throw std::runtime_error("The path " + path_to_save + " already exists");
-	}
 	license::write_file(path_to_save, container);
 	std::cout << "Everything is OK" << std::endl;
-	std::cout << "The request is saved to the file " << path_to_save << std::endl;
+	std::cout << "The request is saved to the file \"" << path_to_save << "\"" << std::endl;
 	std::cout << "Please upload this file to the payment form on the page https://testo-lang.ru/sales" << std::endl;
 	return 0;
 }
