9de7ee1e647c630639b3d98159bb3142d9606166
Sergey Lvov <log0div0@gmail.com>
2020-11-24 13:11:08 +0300

    fix building

--- src/nn/OCR.cpp
+++ src/nn/OCR.cpp
@@ -27,7 +27,7 @@ TextTensor find_text(const stb::Image<stb::RGB>* image) {
 ImgTensor find_img(const stb::Image<stb::RGB>* image, const fs::path& path_to_img) {
 	ImgTensor result;
 
-	result.objects = ImgDetector::instance().detect(image, path_to_img);
+	result.objects = ImgDetector::instance().detect(image, path_to_img.generic_string());
 
 	std::sort(result.objects.begin(), result.objects.end(), [](const Img& a, const Img& b) {
 		return a.rect.top < b.rect.top;
