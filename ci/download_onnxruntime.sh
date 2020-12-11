545e0322416d913a4d1f59a8e9c2946b7fba9715
Sergey Lvov <log0div0@gmail.com>
2020-12-11 19:19:32 +0300

    Переход на onnxruntime 1.5.3, cuda 11.1, cudnn 8.0.5

--- ci/download_onnxruntime.sh
+++ ci/download_onnxruntime.sh
@@ -7,6 +7,9 @@ if [ ! -e "${ONNXRUNTIME_SRC_DIR}" ]; then
 fi
 
 cd "${ONNXRUNTIME_SRC_DIR}"
-git checkout v1.3.0
+git checkout .
+git checkout v1.5.3
 git submodule sync --recursive
 git submodule update --init --recursive
+git apply $SCRIPT_DIR/build_assets/onnxruntime_patches/single_thread.patch
+git apply $SCRIPT_DIR/build_assets/onnxruntime_patches/cuda.patch
