9b108394ec9ca8fa7ba5d77f82f0684342348314
Sergey Lvov <log0div0@gmail.com>
2021-01-02 15:11:16 +0300

    отказываемся от 32х-битной сборки testo

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,8 +3,8 @@
 source "$(dirname "$0")/vars.sh"
 
 ssh -p 22334 root@testo-lang.ru "mkdir /var/www/testo-lang.ru/dist/v${1}"
-scp -P 22334 $OUT_DIR/testo-cpu-x64.msi root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-cpu-x64.msi
-scp -P 22334 $OUT_DIR/testo-cpu-x86.msi root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-cpu-x86.msi
+scp -P 22334 $OUT_DIR/testo-cpu.msi root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-cpu.msi
+scp -P 22334 $OUT_DIR/testo-gpu.msi root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-gpu.msi
 scp -P 22334 $OUT_DIR/testo-cpu.deb root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-cpu.deb
 scp -P 22334 $OUT_DIR/testo-cpu.rpm root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-cpu.rpm
 scp -P 22334 $OUT_DIR/testo-gpu.deb root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-gpu.deb
