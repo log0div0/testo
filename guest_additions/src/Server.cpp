7a402577845cb4f47ffb78a0e73f7664a5cee9c0
Sergey Lvov <log0div0@gmail.com>
2021-01-06 05:39:52 +0300

    Кодировку в fs::filesystem_error не надо править - она там уже исправлена

--- guest_additions/src/Server.cpp
+++ guest_additions/src/Server.cpp
@@ -108,18 +108,20 @@ void Server::handle_command(const nlohmann::json& command) {
 		} else {
 			throw std::runtime_error(std::string("Method ") + method_name + " is not supported");
 		}
-#ifdef WIN32
-	} catch (const std::system_error& error) {
-		spdlog::error("Error in Server::handle_command method");
-		spdlog::error(error.what());
-		std::wstring utf16_err = winapi::acp_to_utf16(error.what());
-		std::string utf8_err = winapi::utf16_to_utf8(utf16_err);
-		send_error(utf8_err);
-#endif
 	} catch (const std::exception& error) {
 		spdlog::error("Error in Server::handle_command method");
 		spdlog::error(error.what());
+#ifdef WIN32
+		if (dynamic_cast<const std::system_error*>(&error) && !dynamic_cast<const fs::filesystem_error*>(&error)) {
+			std::wstring utf16_err = winapi::acp_to_utf16(error.what());
+			std::string utf8_err = winapi::utf16_to_utf8(utf16_err);
+			send_error(utf8_err);
+		} else {
+			send_error(error.what());
+		}
+#else
 		send_error(error.what());
+#endif
 	}
 }
 
--- src/testo/Exceptions.cpp
+++ src/testo/Exceptions.cpp
@@ -8,7 +8,7 @@
 
 void backtrace(std::ostream& stream, const std::exception& error) {
 #ifdef WIN32
-	if (dynamic_cast<const std::system_error*>(&error)) {
+	if (dynamic_cast<const std::system_error*>(&error) && !dynamic_cast<const fs::filesystem_error*>(&error)) {
 		std::wstring utf16_err = winapi::acp_to_utf16(error.what());
 		std::string utf8_err = winapi::utf16_to_utf8(utf16_err);
 		stream << utf8_err;
