77572e5e31717180ccdc2b405328060b43958dc5
Sergey Lvov <log0div0@gmail.com>
2021-01-20 23:29:54 +0300

    build.bat

---
+++ ci/build.bat
@@ -0,0 +1,21 @@
+
+call %~dp0\vars.bat
+
+if "%~1"=="" (
+	set TEST_SPEC=
+) else (
+	set TEST_SPEC=--test_spec %~1
+)
+
+testo run %SCRIPT_DIR%\build_scripts ^
+	--stop_on_fail ^
+	--prefix tb_ ^
+	--param ISO_DIR %ISO_DIR% ^
+	--param TESTO_SRC_DIR %TESTO_SRC_DIR% ^
+	--param BUILD_ASSETS_DIR %SCRIPT_DIR%/build_assets ^
+	--param TMP_DIR %TMP_DIR% ^
+	--param OUT_DIR %OUT_DIR% ^
+	--param ONNXRUNTIME_SRC_DIR %ONNXRUNTIME_SRC_DIR% ^
+	--license %LICENSE_PATH% ^
+	%TEST_SPEC% ^
+	--assume_yes
---
+++ ci/vars.bat
@@ -0,0 +1,8 @@
+
+set SCRIPT_DIR=%~dp0
+set TESTO_SRC_DIR=%SCRIPT_DIR%\..
+set OUT_DIR=%SCRIPT_DIR%\out
+set TMP_DIR=%SCRIPT_DIR%\tmp
+set ISO_DIR=D:\iso
+set ONNXRUNTIME_SRC_DIR=%TESTO_SRC_DIR%\..\onnxruntime
+set LICENSE_PATH=%TESTO_SRC_DIR%\..\testo_license
