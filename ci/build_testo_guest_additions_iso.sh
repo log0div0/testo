ae71b7c25e569b3968b1b593876606bc9b74ac10
Sergey Lvov <log0div0@gmail.com>
2021-02-02 03:15:39 +0300

    сборщик собирает дополнения под все гипервизоры

--- ci/build_testo_guest_additions_iso.sh
+++ ci/build_testo_guest_additions_iso.sh
@@ -2,11 +2,13 @@
 
 source "$(dirname "$0")/vars.sh"
 
-sudo genisoimage -f -J -joliet-long -r -allow-lowercase -allow-multidot \
-	-o ${OUT_DIR}/testo-guest-additions.iso \
-	${SCRIPT_DIR}/build_assets/autorun.inf \
-	${SCRIPT_DIR}/build_assets/autorun.js \
-	${TMP_DIR}/testo-guest-additions.deb \
-	${TMP_DIR}/testo-guest-additions.rpm \
-	${TMP_DIR}/testo-guest-additions-x64.msi \
-	${TMP_DIR}/testo-guest-additions-x86.msi
+for HYPERVISOR in qemu hyperv; do
+	sudo genisoimage -f -J -joliet-long -r -allow-lowercase -allow-multidot \
+		-o ${OUT_DIR}/testo-guest-additions-${HYPERVISOR}.iso \
+		${SCRIPT_DIR}/build_assets/autorun.inf \
+		${SCRIPT_DIR}/build_assets/autorun.js \
+		${TMP_DIR}/${HYPERVISOR}/testo-guest-additions.deb \
+		${TMP_DIR}/${HYPERVISOR}/testo-guest-additions.rpm \
+		${TMP_DIR}/${HYPERVISOR}/testo-guest-additions-x64.msi \
+		${TMP_DIR}/${HYPERVISOR}/testo-guest-additions-x86.msi
+done
