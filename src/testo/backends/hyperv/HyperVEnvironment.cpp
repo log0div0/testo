2ca54b9645c3e8680ff9e501e607d2bf2096e89c
Sergey Lvov <log0div0@gmail.com>
2021-02-11 11:48:58 +0300

    Храним метаданные в program files

--- src/testo/backends/hyperv/HyperVEnvironment.cpp
+++ src/testo/backends/hyperv/HyperVEnvironment.cpp
@@ -1,8 +1,10 @@
 
 #include "HyperVEnvironment.hpp"
+#include <winapi/RegKey.hpp>
 
 fs::path HyperVEnvironment::testo_dir() const {
-	return ".";
+	winapi::RegKey regkey(HKEY_LOCAL_MACHINE, "SOFTWARE\\Testo Lang\\Testo", KEY_QUERY_VALUE);
+	return fs::path(regkey.get_str("InstallDir")) / "metadata";
 }
 
 void HyperVEnvironment::setup() {
