26738b21d7beabc3c3cd480189c6430e15460f89
a.samoylov <cidjey1991@mail.ru>
2021-07-06 13:52:54 +0300

    basic json settings file

--- src/nn_service/main.cpp
+++ src/nn_service/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <stdexcept>
 #include <thread>
 #include <chrono>
@@ -7,17 +8,26 @@
 #include <coro/Acceptor.h>
 #include <coro/StreamSocket.h>
 
+#include <clipp.h>
+#include <nlohmann/json.hpp>
 #include "MessageHandler.hpp"
 
 #include "../nn/OnnxRuntime.hpp"
 
 using namespace std::chrono_literals;
 
+nlohmann::json settings;
+
 void local_handler() {
-	coro::TcpAcceptor acceptor(asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 8888));
+	auto port = settings.value("port", 8156);
+	coro::TcpAcceptor acceptor(asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port));
+	std::cout << "Listening on port " << port << std::endl;
 	acceptor.run([](coro::StreamSocket<asio::ip::tcp> socket) {
 		try {
-			std::cout << "Accepted\n";
+			std::string new_connection = socket.handle().remote_endpoint().address().to_string() +
+				":" + std::to_string(socket.handle().remote_endpoint().port());
+			std::cout << "Accepted new connection: " << new_connection << std::endl;
+
 
 			std::shared_ptr<Channel> channel(new Channel(std::move(socket)));
 
@@ -31,6 +41,27 @@ void local_handler() {
 
 int main(int argc, char** argv) {
 	try {
+		using namespace clipp;
+
+		std::string settings_path;
+
+		auto cli = clipp::group(
+			value("path to settings", settings_path)
+		);
+
+		if (!parse(argc, argv, cli)) {
+			std::cout << make_man_page(cli, argv[0]) << std::endl;
+			return 1;
+		}
+
+		{
+			std::ifstream is(settings_path);
+			if (!is) {
+				throw std::runtime_error(std::string("Can't open settings file: ") + settings_path);
+			}
+			is >> settings;
+		}
+
 		nn::onnx::Runtime onnx_runtime;
 		coro::Application(local_handler).run();
 	} catch (const std::exception& error) {
