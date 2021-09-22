f6bc498ceff2516e03465cfedb7add0b7f3d66e7
Sergey Lvov <log0div0@gmail.com>
2021-09-22 12:23:28 +0300

    fix nn_service init file

--- src/testo_nn_service/pkg/linux/init.d
+++ src/testo_nn_service/pkg/linux/init.d
@@ -2,13 +2,13 @@
 # chkconfig: - 0 99
 
 ### BEGIN INIT INFO
-# Provides: testo-guest-additions
+# Provides: testo-nn-service
 # Required-Start: $remote_fs $syslog
 # Required-Stop: $remote_fs $syslog
 # Default-Start:  2 3 4 5
 # Default-Stop: 0 1 6
-# Short-Description: Testo Guest Additions
-# Description: Guest additions for Testo - a test automation framework
+# Short-Description: Testo NN Service
+# Description: Neural Networks server for Testo - a test automation framework
 ### END INIT INFO
 
 testo-nn-service $@
