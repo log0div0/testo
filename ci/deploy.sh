a0aa06e0f7afb8f647329f24061b68fd86192350
Sergey Lvov <log0div0@gmail.com>
2021-09-29 13:17:43 +0300

    fix deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,12 +3,11 @@
 source "$(dirname "$0")/vars.sh"
 
 ssh -p 22334 root@testo-lang.ru "mkdir /var/www/testo-lang.ru/dist/v${1}"
-#scp -P 22334 $OUT_DIR/testo-cpu.msi root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-cpu.msi
-#scp -P 22334 $OUT_DIR/testo-gpu.msi root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-gpu.msi
 scp -P 22334 $OUT_DIR/testo.deb root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}.deb
+scp -P 22334 $OUT_DIR/testo.rpm root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}.rpm
+scp -P 22334 $OUT_DIR/testo.msi root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}.msi
 scp -P 22334 $OUT_DIR/testo-nn-server.deb root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-nn-server-${1}.deb
-#scp -P 22334 $OUT_DIR/testo-cpu.rpm root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-cpu.rpm
-#scp -P 22334 $OUT_DIR/testo-gpu.deb root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-gpu.deb
-#scp -P 22334 $OUT_DIR/testo-gpu.rpm root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-${1}-gpu.rpm
-#scp -P 22334 $OUT_DIR/testo-guest-additions-hyperv.iso root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-guest-additions-${1}-hyperv.iso
-#scp -P 22334 $OUT_DIR/testo-guest-additions-qemu.iso root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-guest-additions-${1}-qemu.iso
+scp -P 22334 $OUT_DIR/testo-nn-server.rpm root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-nn-server-${1}.rpm
+scp -P 22334 $OUT_DIR/testo-nn-server.msi root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-nn-server-${1}.msi
+scp -P 22334 $OUT_DIR/testo-guest-additions-qemu.iso root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-guest-additions-${1}-qemu.iso
+scp -P 22334 $OUT_DIR/testo-guest-additions-hyperv.iso root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/testo-guest-additions-${1}-hyperv.iso
