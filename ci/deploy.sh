a0e71a253bd5708bac94c8684aad1ff94495617a
Sergey Lvov <log0div0@gmail.com>
2022-01-24 07:52:33 +0300

    deploy links

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -17,3 +17,21 @@ rsync -e "ssh -p 22334" -avz --info=progress2 $OUT_DIR/testo-guest-additions-hyp
 rsync -e "ssh -p 22334" -avz --info=progress2 $OUT_DIR/arm/testo.deb root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/arm/testo-${1}.deb
 rsync -e "ssh -p 22334" -avz --info=progress2 $OUT_DIR/arm/testo-guest-additions-qemu.iso root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/arm/testo-guest-additions-${1}-qemu.iso
 rsync -e "ssh -p 22334" -avz --info=progress2 $OUT_DIR/arm/testo-guest-additions-hyperv.iso root@testo-lang.ru:/var/www/testo-lang.ru/dist/v${1}/arm/testo-guest-additions-${1}-hyperv.iso
+
+echo "
+Windows client (x86_64): https://testo-lang.ru/storage/dist/v${1}/testo-${1}.msi
+Windows server (x86_64): https://testo-lang.ru/storage/dist/v${1}/testo-nn-server-${1}.msi
+Hyper-V guest additions (x86_64): https://testo-lang.ru/storage/dist/v${1}/testo-guest-additions-${1}-hyperv.iso
+Hyper-V guest additions (arm64): https://testo-lang.ru/storage/dist/v${1}/arm/testo-guest-additions-${1}-hyperv.iso
+
+RPM client (x86_64): https://testo-lang.ru/storage/dist/v${1}/testo-${1}.rpm
+RPM server (x86_64): https://testo-lang.ru/storage/dist/v${1}/testo-nn-server-${1}.rpm
+
+DEB client (x86_64): https://testo-lang.ru/storage/dist/v${1}/testo-${1}.deb
+DEB client (arm64): https://testo-lang.ru/storage/dist/v${1}/arm/testo-${1}.deb
+DEB server (x86_64): https://testo-lang.ru/storage/dist/v${1}/testo-nn-server-${1}.deb
+QEMU guest additions (x86_64): https://testo-lang.ru/storage/dist/v${1}/testo-guest-additions-${1}-qemu.iso
+QEMU guest additions (arm64): https://testo-lang.ru/storage/dist/v${1}/arm/testo-guest-additions-${1}-qemu.iso
+" > $OUT_DIR/links.txt
+
+cat $OUT_DIR/links.txt
