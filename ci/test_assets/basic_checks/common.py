6f1a71bf33addd4f50fa3ce976e9414d1f7140e3
Sergey Lvov <log0div0@gmail.com>
2022-01-30 18:44:40 +0300

    test_param_resolve

--- ci/test_assets/basic_checks/common.py
+++ ci/test_assets/basic_checks/common.py
@@ -15,28 +15,32 @@ else:
 	else:
 		raise "Please, specify hypervisor"
 
-def must_succeed(cmd, exit_code = None):
+def must_succeed(cmd, out = None):
 	p = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
-	out, err = p.communicate()
-	out, err = out.decode('utf-8'), err.decode('utf-8')
+	stdout, stderr = p.communicate()
+	stdout, stderr = stdout.decode('utf-8'), stderr.decode('utf-8')
 	print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ", cmd, " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-	print("STDOUT:", out)
-	print("STDERR:", err)
-	assert p.returncode == 0
-	return out, err
+	print("STDOUT:", stdout)
+	print("STDERR:", stderr)
+	assert p.returncode == 0, "returncode == 0"
+	if out is not None:
+		assert out in stdout, f"STDOUT: {out}"
+	return stdout, stderr
 
-def must_fail(cmd, x = None):
+def must_fail(cmd, err = None, out = None):
 	p = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
-	out, err = p.communicate()
-	out, err = out.decode('utf-8'), err.decode('utf-8')
+	stdout, stderr = p.communicate()
+	stdout, stderr = stdout.decode('utf-8'), stderr.decode('utf-8')
 	print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ", cmd, " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-	print("STDOUT:", out)
-	print("STDERR:", err)
-	if x is None:
-		assert p.returncode != 0
-	elif isinstance(x, str):
-		assert p.returncode != 0
-		assert x in err
+	print("STDOUT:", stdout)
+	print("STDERR:", stderr)
+	if err is None:
+		assert p.returncode != 0, "returncode != 0"
+	elif isinstance(err, str):
+		assert p.returncode != 0, "returncode != 0"
+		assert err in stderr, f"STDERR: {err}"
 	else:
-		assert p.returncode == x
-	return out, err
+		assert p.returncode == err, f"returncode == {err}"
+	if out is not None:
+		assert out in stdout, f"STDOUT: {out}"
+	return stdout, stderr
