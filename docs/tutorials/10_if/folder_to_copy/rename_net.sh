df033165698b07aaf05d3bd7992c29ab6ef0cae3
Sergey Lvov <log0div0@gmail.com>
2022-08-21 18:04:50 +0200

    put all tutorials files together

---
+++ docs/tutorials/10_if/folder_to_copy/rename_net.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+mac=$1
+
+oldname=`ifconfig -a | grep ${mac,,} | awk '{print $1}'`
+newname=$2
+
+echo SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"$mac\", NAME=\"$newname\", DRIVERS==\"?*\" >> /lib/udev/rules.d/70-test-tools.rules
+
+rm -f /etc/network/interfaces
+echo source /etc/network/interfaces.d/* >> /etc/network/interfaces
+echo auto lo >> /etc/network/interfaces
+echo iface lo inet loopback >> /etc/network/interfaces
+
+ip link set $oldname down
+ip link set $oldname name $newname
+ip link set $newname up
+
+echo "Renaming success"
